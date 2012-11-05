4f329dc85e6b757599cc26eb40af7646d5f6e453
Emmanuel Bernaszuk <kwizer15@wanadoo.fr>
2012-11-05 15:16:23 +0100

    User : Lien vers une Person

--- src/JLM/UserBundle/Entity/User.php
+++ src/JLM/UserBundle/Entity/User.php
@@ -3,6 +3,7 @@ namespace JLM\UserBundle\Entity;
 
 use FOS\UserBundle\Entity\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use JLM\ModelBundle\Entity\Person;
 
 /**
  * @ORM\Entity
@@ -17,8 +18,36 @@ class User extends BaseUser
      */
     protected $id;
 
+    /**
+     * @ORM\OneToOne(targetEntity="\JLM\ModelBundle\Entity\Person")
+     */
+    private $person;
+    
     public function __construct()
     {
         parent::__construct();
+    }
+
+    /**
+     * Set person
+     *
+     * @param Person $person
+     * @return User
+     */
+    public function setPerson(\JLM\ModelBundle\Entity\Person $person = null)
+    {
+        $this->person = $person;
+    
+        return $this;
+    }
+
+    /**
+     * Get person
+     *
+     * @return JLM\UserBundle\Entity\JLMModelBundle:Person 
+     */
+    public function getPerson()
+    {
+        return $this->person;
     }
 }
