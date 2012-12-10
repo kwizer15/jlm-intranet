c80a07adf3b65327787a4f9b260a8275c8c226e7
Emmanuel Bernaszuk <kwizer15@wanadoo.fr>
2012-12-10 17:38:59 +0100

    Décomposition des objets

---
+++ src/JLM/DailyBundle/Entity/Intervention.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace JLM\DailyBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Plannification d'intervention
+ * JLM\DailyBundle\Entity\InterventionPlanned
+ *
+ * @ORM\Table(name="interventions")
+ * @ORM\Entity
+ */
+class Intervention extends Shifting
+{
+    /**
+     * Porte (lien)
+     * @var JLM\ModelBundle\Entity\Door
+     * @ORM\ManyToOne(targetEntity="JLM\ModelBundle\Entity\Door")
+     */
+    private $door;
+    
+    /**
+     * @var string $contactName
+     * @ORM\Column(name="contact_name", type="string", nullable=true)
+     */
+    private $contactName;
+    
+    /**
+     * @var string $contactPhone
+     * @ORM\Column(name="contact_phones", type="text", nullable=true)
+     */
+    private $contactPhones;
+    
+    /**
+     * E-mail pour envoyer le rapport
+     * @var string $contactEmail
+     * @ORM\Column(name="contact_email", type="text", nullable=true)
+     */
+    private $contactEmail;
+   
+    /**
+     * Set contactName
+     *
+     * @param string $contactName
+     * @return InterventionPlanned
+     */
+    public function setContactName($contactName)
+    {
+        $this->contactName = $contactName;
+    
+        return $this;
+    }
+
+    /**
+     * Get contactName
+     *
+     * @return string 
+     */
+    public function getContactName()
+    {
+        return $this->contactName;
+    }
+
+    /**
+     * Set contactPhones
+     *
+     * @param string $contactPhones
+     * @return InterventionPlanned
+     */
+    public function setContactPhones($contactPhones)
+    {
+        $this->contactPhones = $contactPhones;
+    
+        return $this;
+    }
+
+    /**
+     * Get contactPhones
+     *
+     * @return string 
+     */
+    public function getContactPhones()
+    {
+        return $this->contactPhones;
+    }
+
+    /**
+     * Set contactEmail
+     *
+     * @param string $contactEmail
+     * @return InterventionPlanned
+     */
+    public function setContactEmail($contactEmail)
+    {
+        $this->contactEmail = $contactEmail;
+    
+        return $this;
+    }
+
+    /**
+     * Get contactEmail
+     *
+     * @return string 
+     */
+    public function getContactEmail()
+    {
+        return $this->contactEmail;
+    }
+
+    
+    /**
+     * Set door
+     *
+     * @param JLM\DailyBundle\Entity\Door $door
+     * @return InterventionPlanned
+     */
+    public function setDoor(\JLM\ModelBundle\Entity\Door $door = null)
+    {
+        $this->door = $door;
+    
+        return $this;
+    }
+
+    /**
+     * Get door
+     *
+     * @return JLM\DailyBundle\Entity\Door 
+     */
+    public function getDoor()
+    {
+        return $this->door;
+    }
+
+    
+    
+}
---
+++ src/JLM/DailyBundle/Entity/Shifting.php
@@ -0,0 +1,168 @@
+<?php
+
+namespace JLM\DailyBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Déplacement
+ * JLM\DailyBundle\Entity\InterventionPlanned
+ *
+ * @ORM\Table(name="interventions_planned")
+ * @ORM\Entity
+ * @ORM\InheritanceType("JOINED")
+ * @ORM\DiscriminatorColumn(name="actionType", type="string")
+ * @ORM\DiscriminatorMap({
+ * 		"shifting" = "Shifting",
+ * 		"fixing" = "Fixing",
+ * 		"work" = "Work",
+ * 		"maintenance" = "Maintenance",
+ * 		"receiver" = "Receiver"
+ * 		
+ * })
+ */
+abstract class Shifting
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime $creation
+     *
+     * @ORM\Column(name="creation", type="datetime")
+     */
+    private $creation;
+    
+    /**
+     * Lieu du déplacement
+     * @var string
+     * @ORM\Column(name="place", type="text")
+     */
+    private $place;
+    
+    /**
+     * @var string $reason
+     *
+     * @ORM\Column(name="reason", type="text")
+     */
+    private $reason;
+    
+    /**
+     * Priorité
+     * @var int $priority
+     * 1 - Très Urgent
+     * 2 - Urgent
+     * ....
+     *
+     * @ORM\Column(name="priority", type="smallint")
+     */
+    private $priority;
+    
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set creation
+     *
+     * @param \DateTime $creation
+     * @return InterventionPlanned
+     */
+    public function setCreation($creation)
+    {
+        $this->creation = $creation;
+    
+        return $this;
+    }
+
+    /**
+     * Get creation
+     *
+     * @return \DateTime 
+     */
+    public function getCreation()
+    {
+        return $this->creation;
+    }
+
+    /**
+     * Set reason
+     *
+     * @param string $reason
+     * @return InterventionPlanned
+     */
+    public function setReason($reason)
+    {
+        $this->reason = $reason;
+    
+        return $this;
+    }
+
+    /**
+     * Get reason
+     *
+     * @return string 
+     */
+    public function getReason()
+    {
+        return $this->reason;
+    }
+
+    /**
+     * Set priority
+     *
+     * @param integer $priority
+     * @return InterventionPlanned
+     */
+    public function setPriority($priority)
+    {
+        $this->priority = $priority;
+    
+        return $this;
+    }
+
+    /**
+     * Get priority
+     *
+     * @return integer 
+     */
+    public function getPriority()
+    {
+        return $this->priority;
+    }
+
+    /**
+     * Set place
+     *
+     * @param string $place
+     * @return Shifting
+     */
+    public function setPlace($place)
+    {
+        $this->place = $place;
+    
+        return $this;
+    }
+
+    /**
+     * Get place
+     *
+     * @return string 
+     */
+    public function getPlace()
+    {
+        return $this->place;
+    }
+}
