ee0a05b53179da1ad31a5f28882f5782c1ecf979
Emmanuel Bernaszuk <kwizer15@wanadoo.fr>
2012-12-12 12:13:35 +0100

    Ajout d'un technicien a une intervention

--- src/JLM/DailyBundle/Controller/ShiftingController.php
+++ src/JLM/DailyBundle/Controller/ShiftingController.php
@@ -9,13 +9,19 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use JMS\SecurityExtraBundle\Annotation\Secure;
 use JLM\ModelBundle\Entity\Technician;
 use JLM\DailyBundle\Entity\Shifting;
+use JLM\DailyBundle\Entity\ShiftTechnician;
+use JLM\DailyBundle\Form\Type\AddTechnicianType;
 
-
+/**
+ * Fixing controller.
+ *
+ * @Route("/shifting")
+ */
 class ShiftingController extends Controller
 {
 	/**
-	 * Search
-	 * @Route("/shifting/list/{id}", name="shifting_list")
+	 * List
+	 * @Route("/list/{id}", name="shifting_list")
 	 * @Secure(roles="ROLE_USER")
 	 * @Template()
 	 */
@@ -26,6 +32,55 @@ class ShiftingController extends Controller
 		return array(
 				'technician'=>$technician,
 				'shiftings' => $shiftings
+		);
+	}
+	
+	/**
+	 * Ajoute un technicien sur une intervention
+	 * @Route("/new/{id}", name="shifting_new")
+	 * @Secure(roles="ROLE_USER")
+	 * @Template()
+	 */
+	public function newAction(Shifting $shifting)
+	{
+		$entity = new ShiftTechnician();
+		$form   = $this->createForm(new AddTechnicianType(), $entity);
+		
+		return array(
+				'shifting' => $shifting,
+				'entity' => $entity,
+				'form'   => $form->createView(),
+		);
+	}
+	
+	/**
+	 * Creates a new ShiftTechnician entity.
+	 *
+	 * @Route("/create/{id}", name="shifting_create")
+	 * @Method("POST")
+	 * @Template("JLMDailyBundle:Shifting:new.html.twig")
+	 * @Secure(roles="ROLE_USER")
+	 */
+	public function createAction(Request $request, Shifting $shifting)
+	{
+		$entity  = new ShiftTechnician();
+		$form = $this->createForm(new AddTechnicianType(), $entity);
+		$form->bind($request);
+	
+		if ($form->isValid()) {
+			$em = $this->getDoctrine()->getManager();
+			$entity->setCreation(new \DateTime);
+			$entity->setShifting($shifting);
+			$em->persist($shifting);
+			$em->persist($entity);
+			$em->flush();
+	
+			return $this->redirect($this->generateUrl('daily'));
+		}
+	
+		return array(
+				'entity' => $entity,
+				'form'   => $form->createView(),
 		);
 	}
 }
---
+++ src/JLM/DailyBundle/Form/Type/AddTechnicianType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace JLM\DailyBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class AddTechnicianType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+      		->add('technician',null,array('label'=>'Technicien'))
+      		->add('begin','datepicker',array('label'=>'Date'))
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'JLM\DailyBundle\Entity\ShiftTechnician'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'jlm_dailybundle_addtechniciantype';
+    }
+}
