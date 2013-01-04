81ca99458918f457a5408ddf68ff5488ddf205ac
Emmanuel Bernaszuk <kwizer15@wanadoo.fr>
2013-01-04 14:02:36 +0100

    Fiche rapport de porte

---
+++ src/JLM/DailyBundle/Controller/DoorController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace JLM\DailyBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use JMS\SecurityExtraBundle\Annotation\Secure;
+use JLM\ModelBundle\Entity\Door;
+
+/**
+ * Fixing controller.
+ *
+ * @Route("/door")
+ */
+class DoorController extends Controller
+{
+	/**
+	 * Finds and displays a Door entity.
+	 *
+	 * @Route("/{id}/show", name="daily_door_show")
+	 * @Template()
+	 * @Secure(roles="ROLE_USER")
+	 */
+	public function showAction(Door $door)
+	{
+		$em = $this->getDoctrine()->getManager();
+		$interventions = $em->getRepository("JLMDailyBundle:Intervention")->findByDoor($door,array('creation'=>'DESC'));
+		return array(
+				'entity'      => $door,
+				'interventions' => $interventions,
+		);
+	}
+}
