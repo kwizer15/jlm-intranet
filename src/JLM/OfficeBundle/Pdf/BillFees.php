3bfe711d5e65f6df4f109fca52f3c78da29ab6b7
Emmanuel Bernaszuk <kwizer15@wanadoo.fr>
2013-01-31 11:48:22 +0100

    Correction

--- src/JLM/OfficeBundle/Pdf/BillFees.php
+++ src/JLM/OfficeBundle/Pdf/BillFees.php
@@ -49,7 +49,8 @@ class BillFees extends \FPDF
 		$this->cell(0,5,utf8_decode($this->entity->getTrustee()),0,2);
 		$this->setFont('Arial','',11);
 		$address = $this->entity->getTrustee()->getAddress().'';
-		if ($this->entity->getTrustee()->getBillingAddress()->getCity())
+		if ($this->entity->getTrustee()->getBillingAddress())
+			if ($this->entity->getTrustee()->getBillingAddress()->getStreet())
 			$address = $this->entity->getTrustee()->getBillingAddress().'';
 			
 		$this->multiCell(0,5,utf8_decode($this->entity->getTrustee()->getBillingAddress()));
