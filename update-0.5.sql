ad587e3b728d7d77df2f10504dfd45623ad72029
Emmanuel Bernaszuk <kwizer15@wanadoo.fr>
2013-02-08 10:31:53 +0100

    suppression des fichiers de maj bdd

--- update-0.5.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE shifting_interventions ADD close DATETIME DEFAULT NULL, ADD contract VARCHAR(255) DEFAULT NULL, DROP closed
--- update.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE trustees ADD billingLabel VARCHAR(255) DEFAULT NULL, CHANGE accountNumber accountNumber INT DEFAULT NULL;
-ALTER TABLE doors ADD billing_prelabel LONGTEXT DEFAULT NULL
