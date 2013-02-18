da7a347ea7d33fc91b09f4c59108f71a36ac4dea
Emmanuel Bernaszuk <kwizer15@wanadoo.fr>
2013-02-18 11:20:24 +0100

    Génération automatique des redevances

---
+++ src/JLM/OfficeBundle/Form/Type/FeesType.php
@@ -0,0 +1,30 @@
+<?php
+namespace JLM\OfficeBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class FeesType extends AbstractType
+{
+	public function buildForm(FormBuilderInterface $builder, array $options)
+	{
+		$builder
+			->add('frequence1','percent',array('required'=>false,'attr'=>array('label'=>'Annuelle')))
+			->add('frequence2','percent',array('required'=>false,'attr'=>array('label'=>'Semestrielle')))
+			->add('frequence4','percent',array('required'=>false,'attr'=>array('label'=>'Trimestrielle')))
+		;
+	}
+
+	public function setDefaultOptions(OptionsResolverInterface $resolver)
+	{
+		$resolver->setDefaults(array(
+				'data_class' => 'JLM\OfficeBundle\Entity\FeesFollower'
+		));
+	}
+
+	public function getName()
+	{
+		return 'fees';
+	}
+}
