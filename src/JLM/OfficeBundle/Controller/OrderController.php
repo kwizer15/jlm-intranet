554a9270e512588356c6bed1933139596e9c16f8
Emmanuel Bernaszuk <kwizer15@wanadoo.fr>
2013-02-25 09:47:41 +0100

    Controller pour les commandes

--- src/JLM/OfficeBundle/Controller/OrderController.php
+++ src/JLM/OfficeBundle/Controller/OrderController.php
@@ -10,6 +10,11 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use JMS\SecurityExtraBundle\Annotation\Secure;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use JLM\OfficeBundle\Entity\Order;
+use JLM\OfficeBundle\Entity\OrderLine;
+use JLM\OfficeBundle\Form\OrderType;
+
+
 
 
 
@@ -24,11 +29,134 @@ class OrderController extends Controller
 	 * Lists all Order entities.
 	 *
 	 * @Route("/", name="order")
-	 * @Route("/page/{page}", name="order_page")
 	 * @Template()
 	 * @Secure(roles="ROLE_USER")
 	 */
-	public function indexAction($page = 1)
+	public function indexAction()
 	{
+		$entities = $em->getRepository('JLMOfficeBundle:Quote')->findAll();
+		return array(
+				'entities' => $entities
+				);
+	}
+	
+	/**
+	 * Finds and displays an Order entity.
+	 *
+	 * @Route("/{id}/show", name="order_show")
+	 * @Template()
+	 * @Secure(roles="ROLE_USER")
+	 */
+	public function showAction(Order $entity)
+	{
+		return array('entity'=> $entity);
+	}
+	
+	/**
+	 * Displays a form to create a new Bill entity.
+	 *
+	 * @Route("/new", name="order_new")
+	 *
+	 * @Template()
+	 * @Secure(roles="ROLE_USER")
+	 * Route("/new/quotevariant/{id}", name="order_new_quotevariant")
+	 */
+	public function newAction(QuoteVariant $variant = null)
+	{
+		$entity = new Order();
+		$form   = $this->createForm(new OrderType(), $entity);
+		return array(
+				'entity' => $entity,
+				'form'   => $form->createView()
+		);
+	}
+	
+	/**
+	 * Creates a new Bill entity.
+	 *
+	 * @Route("/create", name="order_create")
+	 * @Method("post")
+	 * @Template("JLMOfficeBundle:Order:new.html.twig")
+	 * @Secure(roles="ROLE_USER")
+	 */
+	public function createAction(Request $request)
+	{
+		$entity  = new Order();
+		$form    = $this->createForm(new OrderType(), $entity);
+		$form->bind($request);
+	
+		if ($form->isValid())
+		{
+			$em = $this->getDoctrine()->getEntityManager();
+			foreach ($entity->getLines() as $line)
+			{
+				$line->setOrder($entity);
+				$em->persist($line);
+			}
+			$em->persist($entity);
+			$em->flush();
+			return $this->redirect($this->generateUrl('order_show', array('id' => $entity->getId())));
+		}
+	
+		return array(
+		'entity' => $entity,
+		'form'   => $form->createView()
+		);
+	}
+	
+	/**
+	 * Displays a form to edit an existing Order entity.
+	 *
+	 * @Route("/{id}/edit", name="order_edit")
+	 * @Template()
+	 * @Secure(roles="ROLE_USER")
+	 */
+	public function editAction(Order $entity)
+	{
+		// Si le devis est déjà validé, on empèche quelconque modification
+		if ($entity->getState() > 2)
+			return $this->redirect($this->generateUrl('order_show', array('id' => $entity->getId())));
+		$editForm = $this->createForm(new OrderType(), $entity);
+		return array(
+				'entity'      => $entity,
+				'edit_form'   => $editForm->createView(),
+		);
+	}
+	
+	/**
+	 * Edits an existing Order entity.
+	 *
+	 * @Route("/{id}/update", name="order_update")
+	 * @Method("post")
+	 * @Template("JLMOfficeBundle:Order:edit.html.twig")
+	 * @Secure(roles="ROLE_USER")
+	 */
+	public function updateAction(Request $request, Order $entity)
+	{
+		 
+		// Si la commande est déjà validé, on empèche quelconque modification
+		if ($entity->getState() > 2)
+			return $this->redirect($this->generateUrl('order_show', array('id' => $entity->getId())));
+	
+		$editForm = $this->createForm(new OrderType(), $entity);
+		$editForm->bind($request);
+	
+		if ($editForm->isValid())
+		{
+			$em = $this->getDoctrine()->getEntityManager();
+			foreach ($entity->getLines() as $line)
+			{
+				$line->setOrder($entity);
+				$em->persist($line);
+			}
+			$em->persist($entity);
+			$em->flush();
+			return $this->redirect($this->generateUrl('order_show', array('id' => $entity->getId())));
+		}
+	
+		return array(
+				'entity'      => $entity,
+				'edit_form'   => $editForm->createView(),
+		);
 	}
 }
