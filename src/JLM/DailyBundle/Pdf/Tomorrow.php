df3e346d399d363aeda316e511ce31b21ff7e3b4
manu <manu@jlm-9>
2013-04-08 17:18:47 +0200

    Ajout du contact dans Demain

--- src/JLM/DailyBundle/Pdf/Tomorrow.php
+++ src/JLM/DailyBundle/Pdf/Tomorrow.php
@@ -29,11 +29,12 @@ class Tomorrow extends \FPDF
 		$this->cell(0,12,'Interventions du '.$date->format('d/m/Y'),1,1,'C',true);
 		$this->ln(5);
 		$this->setFont('Arial','B',11);
-		$this->cell(30,6,'Type',1,0,'L',true);
+		$this->cell(23,6,'Type',1,0,'L',true);
 		$this->cell(77,6,'Affaire',1,0,'L',true);
-		$this->cell(10,6,'Ctr',1,0,'L',true);
-		$this->cell(120,6,'Raison',1,0,'L',true);
-		$this->cell(40,6,'Technicien',1,1,'L',true);
+		$this->cell(8,6,'Ctr',1,0,'L',true);
+		$this->cell(109,6,'Raison',1,0,'L',true);
+		$this->cell(30,6,'Contact',1,0,'L',true);
+		$this->cell(30,6,'Technicien',1,1,'L',true);
 		$this->setFont('Arial','',10);
 	}
 	
@@ -46,7 +47,7 @@ class Tomorrow extends \FPDF
 				'work' => 'Travaux',
 		);
 		$xorig = $this->getX();
-		$this->cell(30,5,$types[$entity->getType()],0,0,'L');
+		$this->cell(23,5,$types[$entity->getType()],0,0,'L');
 		$x = $this->getX();
 		$yorig = $this->getY();
 		if ($entity->getType() != 'equipment')
@@ -75,30 +76,40 @@ class Tomorrow extends \FPDF
 		if ($entity->getType() != 'equipment')
 		{
 			if ($entity->getContract() == 'Hors contrat')
-				$this->cell(10,5,'HC',0,0,'L');
+				$this->cell(8,5,'HC',0,0,'L');
 			else
-				$this->cell(10,5,$entity->getContract(),0,0,'L');
+				$this->cell(8,5,$entity->getContract(),0,0,'L');
 		}
 		else 
-			$this->cell(10,5,'',1,0,'L');
+			$this->cell(8,5,'',1,0,'L');
 		$x = $this->getX();
 		$ymax = max($ymax,$this->getY());
-		$this->multicell(120,5,$entity->getReason(),0,'L');
-		$x += 120;
+		$this->multicell(109,5,$entity->getReason(),0,'L');
+		$x += 109;
+		$this->setXY($x,$yorig);
+		if ($entity->getType() != 'equipment')
+		{
+			$this->cell(30,5,$entity->getContactName(),0,2,'L');
+			$this->multicell(30,5,$entity->getContactPhones(),0,'L');
+		}
+		
+		$ymax = max($ymax,$this->getY());
+		$x += 30;
 		$this->setXY($x,$yorig);
 		foreach($entity->getShiftTechnicians() as $tech)
 		{
 			if ($tech->getBegin() == $date)
-				$this->cell(40,5,$tech->getTechnician(),0,2,'L');
+				$this->cell(30,5,$tech->getTechnician(),0,2,'L');
 		}
 		$ymax = max($ymax,$this->getY());
 		$this->setXY($xorig,$yorig);
 		$ysize = $ymax - $yorig;
-		$this->cell(30,$ysize,'',1,0);
+		$this->cell(23,$ysize,'',1,0);
 		$this->cell(77,$ysize,'',1,0);
-		$this->cell(10,$ysize,'',1,0);
-		$this->cell(120,$ysize,'',1,0);
-		$this->cell(40,$ysize,'',1,1);	
+		$this->cell(8,$ysize,'',1,0);
+		$this->cell(109,$ysize,'',1,0);
+		$this->cell(30,$ysize,'',1,0);
+		$this->cell(30,$ysize,'',1,1);	
 	}
 	
 	public function cell($w, $h = 0, $txt = '', $border = 0, $ln = 0, $align = '', $fill = false, $link = '')
