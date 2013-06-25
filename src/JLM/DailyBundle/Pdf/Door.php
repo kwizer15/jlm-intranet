d3a79f8780426d2c8aa4f18ca1b1cd91d4d913f6
manu <manu@jlm-9>
2013-06-25 16:48:37 +0200

    Correction taille des colonnes

--- src/JLM/DailyBundle/Pdf/Door.php
+++ src/JLM/DailyBundle/Pdf/Door.php
@@ -31,7 +31,7 @@ class Door extends FPDFext
 		$this->multicell(0,12,$door->toString(),1,1,'C',true);
 		$this->ln(5);
 		$this->setFont('Arial','B',11);
-		$this->setWidths(array(24,77,8,69,70,29));
+		$this->setWidths(array(24,24,8,96,96,29));
 		$this->row(array('Date','Type','Ctr','Raison','Rapport','Technicien'),6,1,true);
 		$this->setFont('Arial','',10);
 	}
@@ -47,10 +47,11 @@ class Door extends FPDFext
 		$datas[0] = $entity->getBegin()->format('d/m/Y');
 		$datas[1] = $types[$shifting->getType()];
 		$datas[2] = ($shifting->getContract() == 'Hors contrat') ? 'HC' : $shifting->getContract();
-		$datas[4] = $shifting->getReport();
+		$datas[3] = $shifting->getReason();
+		$datas[4] = 'rapport : '.$shifting->getReport();
 		if ($shifting->getRest())
 			$datas[4] .= chr(10).chr(10).'Reste à faire :'.chr(10).$shifting->getRest();
-		$datas[3] = $shifting->getReason();
+		
 		$datas[5] = $entity->getTechnician().'';
 		$this->row($datas,5,1,false);
 	}
