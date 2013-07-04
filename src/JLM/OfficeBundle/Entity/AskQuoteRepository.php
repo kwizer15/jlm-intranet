7045b9675af94f65a3c00c1631639a761f465b69
manu <manu@jlm-9>
2013-07-04 13:39:02 +0200

    Changement d'ordre (par date)

--- src/JLM/OfficeBundle/Entity/AskQuoteRepository.php
+++ src/JLM/OfficeBundle/Entity/AskQuoteRepository.php
@@ -74,7 +74,7 @@ class AskQuoteRepository extends EntityRepository
 			->leftJoin('a.intervention','c')
 			->where('b is null')
 			->andWhere('a.dontTreat is null')
-			->orderBy('c.creation','asc')
+			->orderBy('a.creation','asc')
 			;
 		return $qb->getQuery()->getResult();
 	}
@@ -87,7 +87,7 @@ class AskQuoteRepository extends EntityRepository
 			->leftJoin('a.intervention','c')
 			->where('b is not null')
 			->andWhere('a.dontTreat is not null')
-			->orderBy('c.creation','asc')
+			->orderBy('a.creation','asc')
 		;
 		return $qb->getQuery()->getResult();
 	}
