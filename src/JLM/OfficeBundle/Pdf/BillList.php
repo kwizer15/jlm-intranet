e6ce7a2821d08efb7ea43d2aa4614470e669a256
manu <manu@jlm-9>
2013-09-23 16:36:11 +0200

    Ajout du syndic dans la liste des factures

--- src/JLM/OfficeBundle/Pdf/BillList.php
+++ src/JLM/OfficeBundle/Pdf/BillList.php
@@ -33,11 +33,11 @@ class BillList extends FPDFext
 	public function _header()
 	{
 		$this->setFont('Arial','B',18);
-		$this->multicell(0,12,'Liste des intervanetions à facturer',1,1,'C',true);
+		$this->multicell(0,12,'Liste des interventions à facturer',1,1,'C',true);
 		$this->ln(5);
 		$this->setFont('Arial','B',11);
-		$this->setWidths(array(24,63,24,8,79,79)); // -29
-		$this->row(array('Date','Affaire','Type','Ctr','Raison','Rapport'),6,1,true);
+		$this->setWidths(array(24,30,58,24,8,64,69)); // -29
+		$this->row(array('Date','Syndic','Affaire','Type','Ctr','Raison','Rapport'),6,1,true);
 		$this->setFont('Arial','',10);
 	}
 	
@@ -50,13 +50,14 @@ class BillList extends FPDFext
 		);
 		$dayTrans = array('dimanche','lundi','mardi','mercredi','jeudi','vendredi','samedi');
 		$datas[0] = $dayTrans[$entity->getClose()->format('w')].chr(10).$entity->getClose()->format('d/m/Y');
-		$datas[1] = $entity->getDoor()->getSite()->getAddress().'';
-		$datas[2] = $types[$entity->getType()];
-		$datas[3] = ($entity->getContract() == 'Hors contrat') ? 'HC' : $entity->getDynContract();
-		$datas[4] = $entity->getReason();
-		$datas[5] = 'Rapport :'.chr(10).$entity->getReport();
+		$datas[1] = $entity->getDoor()->getTrustee();
+		$datas[2] = $entity->getDoor()->getSite()->getAddress().'';
+		$datas[3] = $types[$entity->getType()];
+		$datas[4] = ($entity->getContract() == 'Hors contrat') ? 'HC' : $entity->getDynContract();
+		$datas[5] = $entity->getReason();
+		$datas[6] = 'Rapport :'.chr(10).$entity->getReport();
 		if ($entity->getRest())
-			$datas[5] .= chr(10).chr(10).'Reste à faire :'.chr(10).$entity->getRest();
+			$datas[6] .= chr(10).chr(10).'Reste à faire :'.chr(10).$entity->getRest();
 		$this->row($datas,5,1,false);
 	}
 
