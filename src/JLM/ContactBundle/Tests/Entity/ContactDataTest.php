1904fc9596d768c1321a0e3b8f2de35ad1db622a
Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
2013-11-26 16:03:44 +0100

    Remaniement de ContactDataTest

--- src/JLM/ContactBundle/Tests/Entity/ContactDataTest.php
+++ src/JLM/ContactBundle/Tests/Entity/ContactDataTest.php
@@ -6,6 +6,11 @@ use JLM\ContactBundle\Entity\ContactDataException;
 
 class ContactDataTest extends \PHPUnit_Framework_TestCase
 {
+	public function setUp()
+	{
+		$this->entity = $this->getMockForAbstractClass('JLM\ContactBundle\Entity\ContactData');
+	}
+	
 	public function providerAlias()
 	{
 		return array(
@@ -15,39 +20,58 @@ class ContactDataTest extends \PHPUnit_Framework_TestCase
 			array('  n\'importe-quoi  encore  ','N\'importe-quoi encore'),
 			array('une chaine de caractère super longue qui doit faire au moins 255 caractères pour que ca bug au niveau de la base de données une chaine de caractère super longue qui doit faire au moins 255 caractères pour que ca bug au niveau de la base de données ggggggg',
 					  'Une chaine de caractère super longue qui doit faire au moins 255 caractères pour que ca bug au niveau de la base de données une chaine de caractère super longue qui doit faire au moins 255 caractères pour que ca bug au niveau de la base de données g'),
-			array(''),
 			array(12,'12'),
 			array(true,'1'),
-			array(false),
-			array(array()),
-			array(new \stdClass),
-	
 		);
 	}
 	
+	public function providerAliasException()
+	{
+		return array(
+				array(''),
+				array(false),
+				array(array()),
+				array(new \stdClass),
+		);
+	}
+	
+	/**
+	 * @test
+	 */
 	public function testInitialAlias()
 	{
-		$entity = $this->getMockForAbstractClass('JLM\ContactBundle\Entity\ContactData');
-		$this->assertSame('',$entity->getAlias());
+		$this->assertSame('',$this->entity->getAlias());
 	} 
 	
 	/**
 	 * @test
 	 * @dataProvider providerAlias
 	 */
-	public function testAlias($in,$out = 'Exception')
+	public function testSetAlias($in)
 	{
-		$entity = $this->getMockForAbstractClass('JLM\ContactBundle\Entity\ContactData');
-
-		try {
-			$this->assertEquals($entity,$entity->setAlias($in));
-			if ($out === 'Exception')
-				$this->fail('Une exception attendue n\'a pas été levée');
-			else
-				$this->assertSame($out,$entity->getAlias());
-		} catch (ContactDataException $e) {
-			if ($out !== 'Exception')
-				$this->fail('Une exception non attendue a été levée');
-		}	
+		$this->assertEquals($this->entity,$this->entity->setAlias($in));
 	}
+	
+	/**
+	 * @test
+	 * @dataProvider providerAliasException
+	 * @expectedException JLM\ContactBundle\Entity\ContactDataException
+	 */
+	public function testSetAliasException($in)
+	{
+		$this->entity->setAlias($in);
+	}
+	
+	/**
+	 * @test
+	 * @dataProvider providerAlias
+	 * @depends testSetAlias
+	 */
+	public function testGetAlias($in,$out)
+	{
+		$this->entity->setAlias($in);
+		$this->assertSame($out,$this->entity->getAlias());
+	}
+	
+	
 }
