1783bf79dc006999e2a107b8df91b9fc89f27f70
Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
2013-12-02 16:59:07 +0100

    Suppression du constructeur dans l'interface d'Email

--- src/JLM/ContactBundle/Entity/EmailInterface.php
+++ src/JLM/ContactBundle/Entity/EmailInterface.php
@@ -4,12 +4,6 @@ namespace JLM\ContactBundle\Entity;
 interface EmailInterface
 {
 	/**
-	 * Constructor
-	 * @param email
-	 */
-	public function __construct($address = null);
-	
-	/**
 	 * To String
 	 * @return string
 	 */
