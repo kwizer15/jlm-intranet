1ab7519308fd582d869b44d03c9ad5fdad09768d
Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
2013-12-18 16:14:24 +0100

    Remise à zéro du DefaultController de Fee

--- src/JLM/FeeBundle/Controller/DefaultController.php
+++ src/JLM/FeeBundle/Controller/DefaultController.php
@@ -17,22 +17,4 @@ class DefaultController extends Controller
     {
         return array('name' => $name);
     }
-    
-    /**
-     * @Route("/generate")
-     */
-    public function generateAction(FeesFollower $follower)
-    {
-    	$em = $this->getDoctrine()->getEntityManager();
-    	
-    	// On prend toutes les redevances
-    	$fees = $em->getRepository('JLMFeeBundle:Fee')->findAll();
-    	
-    	// On genère les factures
-    	foreach ($fees as $fee)
-    	{
-    		$bill = $fee->createBill($follower, new Bill);
-    	}
-    		
-    }
 }
