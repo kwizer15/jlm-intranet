ea0b3ca47faac7f73e60795aeb4132d61e38be60
Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
2013-12-20 12:12:38 +0100

    Transformer pour HiddenContact

---
+++ src/JLM/ContactBundle/Form/DataTransformer/ContactToIntTransformer.php
@@ -0,0 +1,56 @@
+<?php
+namespace JLM\ContactBundle\Form\DataTransformer;
+
+use Symfony\Component\Form\DataTransformerInterface;
+use Symfony\Component\Form\Exception\TransformationFailedException;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class ContactToIntTransformer implements DataTransformerInterface
+{
+	/**
+	 * @var ObjectManager
+	 */
+	private $om;
+	
+	/**
+	 * @param ObjectManager $om
+	 */
+	public function __construct(ObjectManager $om)
+	{
+		$this->om = $om;
+	}
+	
+	/**
+	 * {@inheritdoc}
+	 */
+	public function transform($entity)
+	{
+		if (null === $entity) {
+			return '';
+		}
+		return $entity->getId();
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function reverseTransform($id)
+	{
+		if (!$id)
+			return null;
+	
+		
+		$entity = $this->om
+				->getRepository('JLMContactBundle:Contact')
+				->find($id)
+			;
+		if (null === $entity) {
+			throw new TransformationFailedException(sprintf(
+					'A contact with id "%s" does not exist!',
+					$id
+			));
+		}
+	
+		return $entity;
+	}
+}
