7f07fd4d1ec576f3548ece0f9a1bc2c06b4fd136
Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
2014-01-21 17:08:46 +0100

    Modale automatique avec le type de formulaire

---
+++ src/JLM/ContactBundle/DependencyInjection/Compiler/FormPass.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of the GenemuFormBundle package.
+ *
+ * (c) Olivier Chauvel <olivier@generation-multiple.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace JLM\ContactBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+/**
+ * Add a new twig.form.resources
+ *
+ * @author Olivier Chauvel <olivier@generation-multiple.com>
+ */
+class FormPass implements CompilerPassInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function process(ContainerBuilder $container)
+    {
+        $resources = $container->getParameter('twig.form.resources');
+		$resources[] = 'JLMContactBundle:Form:modal_layout.html.twig';
+        $container->setParameter('twig.form.resources', $resources);
+    }
+}
