e040a3de3af8d6cc853f1fc07026525149926aae
Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
2014-01-21 11:42:48 +0100

    Création du champ SelectContact

---
+++ src/JLM/ContactBundle/Form/Type/SelectContactType.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace JLM\ContactBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use JLM\ContactBundle\Form\DataTransformer\ContactToIntTransformer;
+
+class SelectContactType extends AbstractType
+{
+	/**
+	 * @var ObjectManager
+	 */
+	private $om;
+	
+	/**
+	 * @param ObjectManager $om
+	 */
+	public function __construct(ObjectManager $om)
+	{
+		$this->om = $om;
+	}
+	
+	/**
+	 * @param FormBuilderInterface $builder
+	 * @param array $options
+	 */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+    	$transformer = new ContactToIntTransformer($this->om);
+    	$builder->addModelTransformer($transformer);
+    	
+    }
+
+    public function getParent()
+    {
+    	return 'genemu_jqueryselect2_entity';
+    }
+    
+    public function getName()
+    {
+        return 'selectcontacttype';
+    }
+    
+	public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'invalid_message' => 'The selected contact does not exist',
+        	'class' => 'JLM\ContactBundle\Entity\Contact',
+        	'attr' => array('class'=>'input-xlarge'),
+        	'empty_value' => '',
+        	'configs' => array(
+        			'placeholder' => 'Sélectionner un contact...',
+        		),
+        ));
+    }
+}
