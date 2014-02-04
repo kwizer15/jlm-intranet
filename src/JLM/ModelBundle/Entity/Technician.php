0e5d421881072f136784cccd1a5a5f08722cac79
Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
2014-02-04 10:20:31 +0100

    Correction message d'erreur

--- src/JLM/ModelBundle/Entity/Technician.php
+++ src/JLM/ModelBundle/Entity/Technician.php
@@ -17,7 +17,7 @@ class Technician extends Person
 	 * @var string $internalPhone
 	 *
 	 * @ORM\Column(name="internalPhone",type="string", length=20, nullable=true)
-	 * @Assert\Regex(pattern="/^\d{3,4}$/",message="Ce numéro ne contient pas uniquement 10 chiffres")
+	 * @Assert\Regex(pattern="/^\d{3,4}$/",message="Ce numéro ne contient pas uniquement 3 à 4 chiffres")
 	 */
 	private $internalPhone;
 	
