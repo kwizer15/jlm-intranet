ce783111eee66c71147efdb5049905fefba48145
Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
2014-02-06 12:11:47 +0100

    Implementation de prix

---
+++ src/JLM/ProductBundle/Entity/PriceEuro.php
@@ -0,0 +1,59 @@
+<?php
+namespace JLM\ProductBundle\Entity;
+
+use JLM\ProductBundle\Model\PriceInterface;
+
+class PriceEuro implements PriceInterface
+{
+	/**
+	 * 
+	 * @var decimal $value
+	 */
+	private $value;
+	
+	/**
+	 * Constructor
+	 * 
+	 * @param decimal $value
+	 */
+	public function __construct($value)
+	{
+		$this->setValue($value);
+	}
+	
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getValue()
+	{
+		return $this->value;
+	}
+
+	/**
+	 * Set the value
+	 * 
+	 * @param decimal $value
+	 * @return self
+	 */
+	public function setValue($value)
+	{
+		$this->value = $value;
+		return $this;
+	}
+	
+	/**
+	 * {@inheritdoc}
+	*/
+	public function getCurrency()
+	{
+		return '€';
+	}
+
+	/**
+	 * {@inheritdoc}
+	*/
+	public function __toString()
+	{
+		return $this->getValue().' '.$this->getCurrency();
+	}
+}
