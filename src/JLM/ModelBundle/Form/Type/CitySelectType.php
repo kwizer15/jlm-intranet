11a0bd16605adeeada4a947ff61d53fb71f95926
Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
2014-03-13 11:37:33 +0100

    Correction CitySelectType (data_class)

--- src/JLM/ModelBundle/Form/Type/CitySelectType.php
+++ src/JLM/ModelBundle/Form/Type/CitySelectType.php
@@ -10,34 +10,9 @@ use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class CitySelectType extends AbstractType
 {
-//	/**
-//	 * @var ObjectManager
-//	 */
-//	private $om;
-//	
-//	/**
-//	 * @param ObjectManager $om
-//	 */
-//	public function __construct(ObjectManager $om)
-//	{
-//		$this->om = $om;
-//	}
-//	
-//	/**
-//	 * @param FormBuilderInterface $builder
-//	 * @param array $options
-//	 */
-//    public function buildForm(FormBuilderInterface $builder, array $options)
-//    {
-//    	$transformer = new CityToStringTransformer($this->om);
-//    	$builder->addModelTransformer($transformer);
-//    	
-//    }
-//
     public function getParent()
     {
     	return 'genemu_jqueryselect2_hidden';
-//    	return 'text';
     }
     
     public function getName()
@@ -48,8 +23,7 @@ class CitySelectType extends AbstractType
 	public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
         $resolver->setDefaults(array(
-//            'invalid_message' => 'The selected city does not exist',
-			'class' => 'JLM\ModelBundle\Entity\City'
+			'data_class' => 'JLM\ModelBundle\Entity\City'
         ));
     }
 }
