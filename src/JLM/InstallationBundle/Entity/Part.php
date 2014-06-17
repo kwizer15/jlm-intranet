5028f84c1b3ec9aae50bdf32c669dd410c0dcb8e
Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
2014-06-17 12:26:58 +0200

    Part

---
+++ src/JLM/InstallationBundle/Entity/Part.php
@@ -0,0 +1,110 @@
+<?php
+
+/*
+ * This file is part of the installation-bundle package.
+ *
+ * (c) Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace JLM\InstallationBundle\Entity;
+
+use JLM\InstallationBundle\Model\PartInterface;
+use JLM\InstallationBundle\Model\PartTypeInterface;
+use JLM\InstallationBundle\Model\InstallationInterface;
+use JLM\InstallationBundle\Model\PartStateInterface;
+/**
+ * @author Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
+ */
+class Part implements PartInterface
+{
+    /**
+     * Localisation
+     * @var string
+     */
+    private $location;
+    
+    /**
+     * Installation
+     * @var InstallationInterface
+     */
+    private $installation;
+    
+    /**
+     * Type de la pièce
+     * @var PartTypeInterface
+     */
+    private $type;
+    
+    /**
+     * 
+     * @param string $name
+     * @param PartCategoryInterface $category
+     * @param array|Traversable $states
+     */
+    public function __construct(InstallationInterface $installation, PartTypeInterface $type, $location)
+    {
+        $this->setInstallation($installation);
+        $this->setType($type);
+        $this->setLocation($location);
+    }
+    
+    /**
+     * @param string $location
+     * @return self
+     */
+    public function setLocation($location)
+    {
+        $this->location = $location;
+    
+        return $this;
+    }
+    
+    /**
+     * @return string
+     */
+    public function getLocation()
+    {
+        return $this->location;
+    }
+    
+    /**
+     * @param PartTypeInterface $type
+     * @return self
+     */
+    public function setType(PartTypeInterface $type)
+    {
+        $this->type = $type;
+    
+        return $this;
+    }
+    
+    /** 
+     * @return PartTypeInterface
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+    
+    /**
+     * @param InstallationInterface $installation
+     * @return self
+     */
+    public function setInstallation(InstallationInterface $installation)
+    {
+        $this->installation = $installation;
+    
+        return $this;
+    }
+    
+    /**
+     * @return InstallationInterface
+     */
+    public function getInstallation()
+    {
+        return $this->installation;
+    }
+}
---
+++ src/JLM/InstallationBundle/Tests/Entity/PartTest.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ * This file is part of the installation-bundle package.
+ *
+ * (c) Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace JLM\InstallationBundle\Tests\Entity;
+
+/**
+ * @author Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
+ */
+class PartTest extends \PHPUnit_Framework_TestCase
+{
+    private $entity;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp()
+    {
+        $this->partType = $this->getMock('JLM\InstallationBundle\Model\PartTypeInterface');
+        $this->installation = $this->getMock('JLM\InstallationBundle\Model\InstallationInterface');
+        $this->entity = new \JLM\InstallationBundle\Entity\Part($this->installation, $this->partType, 'foo');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function assertPreConditions()
+    {
+        $this->assertInstanceOf('JLM\InstallationBundle\Model\PartInterface', $this->entity);
+        $this->assertSame($this->partType, $this->entity->getType());
+        $this->assertSame($this->installation, $this->entity->getInstallation());
+        $this->assertSame('foo', $this->entity->getLocation());
+    }
+
+    public function testGetType()
+    {
+        $this->assertSame($this->partType, $this->entity->getType());
+    }
+
+    public function testGetInstallation()
+    {
+        $this->assertSame($this->installation, $this->entity->getInstallation());
+    }
+    
+    public function testGetLocation()
+    {
+        $this->assertSame('foo', $this->entity->getLocation());
+    }
+}
