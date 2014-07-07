e74493287c835ad9785d1c47d0c189eef10e13de
Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
2014-07-07 09:42:48 +0200

    Add test to BillBuilderAbstractTest

--- src/JLM/BillBundle/Tests/Builder/BillBuilderAbstractTest.php
+++ src/JLM/BillBundle/Tests/Builder/BillBuilderAbstractTest.php
@@ -31,6 +31,7 @@ class BillBuilderAbstractTest extends \PHPUnit_Framework_TestCase
      */
     protected function assertPreConditions()
     {
+        $this->assertInstanceOf('JLM\BillBundle\Builder\BillBuilderInterface', $this->builder);
         $this->builder->create();
     }
     
