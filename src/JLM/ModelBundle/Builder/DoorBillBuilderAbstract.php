50258f33f4833d52ad6d88229ae54bd1c1e1f72f
Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
2014-07-08 12:09:34 +0200

    Add SiteBillBuilderAbstract Factoring DoorSiteBuilderAbstract

--- src/JLM/ModelBundle/Builder/DoorBillBuilderAbstract.php
+++ src/JLM/ModelBundle/Builder/DoorBillBuilderAbstract.php
@@ -11,35 +11,28 @@
 
 namespace JLM\ModelBundle\Builder;
 
-use JLM\BillBundle\Builder\BillBuilderAbstract;
 /**
  * @author Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
  */
-abstract class DoorBillBuilderAbstract extends BillBuilderAbstract
+abstract class DoorBillBuilderAbstract extends SiteBillBuilderAbstract
 {
     abstract protected function _getDoor();
     
+    /**
+     * {@inheritdoc}
+     */
     protected function _getSite()
     {
         return $this->_getDoor()->getSite();
     }
     
+    /**
+     * {@inheritdoc}
+     */
     protected function _getTrustee()
     {
         $contract = $this->_getDoor()->getActualContract();
         return (empty($contract)) ? $this->_getDoor()->getSite()->getTrustee() : $contract->getTrustee();
-    }
-   
-    /**
-     * {@inheritdoc}
-     */
-    public function buildBusiness()
-    {
-        $site = $this->_getSite();
-        $this->getBill()->setSite($site->toString());
-        $this->getBill()->setSiteObject($site);
-        $this->getBill()->setPrelabel($site->getBillingPrelabel());
-        $this->getBill()->setVat($site->getVat()->getRate());
     }
     
     /**
@@ -49,17 +42,5 @@ abstract class DoorBillBuilderAbstract extends BillBuilderAbstract
     {
         $door = $this->_getDoor();
         $this->getBill()->setDetails($door->getType().' - '.$door->getLocation());
-    }
-    
-    /**
-     * {@inheritdoc}
-     */
-    public function buildCustomer()
-    {
-        $trustee = $this->_getTrustee();
-        $this->getBill()->setTrustee($trustee);
-        $this->getBill()->setTrusteeName($trustee->getBillingLabel());
-        $this->getBill()->setTrusteeAddress($trustee->getAddressForBill()->toString());
-        $this->getBill()->setAccountNumber(($trustee->getAccountNumber() == null) ? '411000' : $trustee->getAccountNumber());
     }
 }
