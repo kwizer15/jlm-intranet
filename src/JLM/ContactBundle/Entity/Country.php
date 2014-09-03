d5672f92f4c723293424420acbacfc33fda5a4ff
Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
2014-09-03 15:18:46 +0200

    Correction on CountryEntity

--- src/JLM/ContactBundle/Entity/Country.php
+++ src/JLM/ContactBundle/Entity/Country.php
@@ -1,6 +1,15 @@
 <?php
 
-namespace JLM\ModelBundle\Entity;
+/*
+ * This file is part of the JLMContactBundle package.
+*
+* (c) Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+namespace JLM\ContactBundle\Entity;
 
 use JLM\ContactBundle\Model\CountryInterface;
 
