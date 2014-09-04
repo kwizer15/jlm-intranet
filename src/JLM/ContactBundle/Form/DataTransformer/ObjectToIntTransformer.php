f09b2dfe410694c6b5c744ad48fb72242e84e8f0
Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
2014-09-04 15:06:14 +0200

    Add CitySelectType

---
+++ src/JLM/ContactBundle/Form/DataTransformer/ObjectToIntTransformer.php
@@ -0,0 +1,81 @@
+<?php
+
+/*
+ * This file is part of the JLMContactBundle package.
+ *
+ * (c) Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace JLM\ContactBundle\Form\DataTransformer;
+
+use Symfony\Component\Form\DataTransformerInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Form\Exception\TransformationFailedException;
+
+/**
+ * @author Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
+ */
+abstract class ObjectToIntTransformer implements DataTransformerInterface
+{
+	/**
+	 * @var ObjectManager
+	 */
+	private $om;
+	
+	/**
+	 * @param ObjectManager $om
+	 */
+	public function __construct(ObjectManager $om)
+	{
+		$this->om = $om;
+	}
+	
+	/**
+	 * {@inheritdoc}
+	 */
+	public function transform($entity)
+	{
+		return (null === $entity) ? '' : $entity->getId();
+	}
+	
+	/**
+	 * {@inheritdoc}
+	 */
+	public function reverseTransform($id)
+	{
+		if (!$id)
+		{
+			return null;
+		}
+		$entity = $this->om
+		  ->getRepository($this->getClass())
+		  ->find($id)
+		;
+		if (null === $entity)
+		{
+			throw new TransformationFailedException(sprintf($this->_getErrorMessage(), $id));
+		}
+	
+		return $entity;
+	}
+	
+	/**
+	 * Return class name
+	 * 
+	 * @return string
+	 */
+	abstract protected function _getClass();
+	
+	/**
+	 * Get error message
+	 * 
+	 * @return string
+	 */
+	protected function _getErrorMessage()
+	{
+		return 'A '.$this->_getClass().' object with id "%s" does not exist!';
+	}
+}
---
+++ src/JLM/ContactBundle/Form/Type/CitySelectType.php
@@ -0,0 +1,62 @@
+<?php
+
+/*
+ * This file is part of the JLMContactBundle package.
+ *
+ * (c) Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace JLM\ContactBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use JLM\ContactBundle\Form\DataTransformer\CityToIntTransformer;
+
+/**
+ * @author Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
+ */
+class CitySelectType extends AbstractType
+{
+    /**
+     * @var ObjectManager
+     */
+    private $om;
+    
+    /**
+     * @param ObjectManager $om
+     */
+    public function __construct(ObjectManager $om)
+    {
+        $this->om = $om;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function getParent()
+    {
+    	return 'genemu_jqueryselect2_hidden';
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'jlm_contact_city_select';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+	public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'transformer' => new CityToIntTransformer($this->om),
+        ));
+    }
+}
