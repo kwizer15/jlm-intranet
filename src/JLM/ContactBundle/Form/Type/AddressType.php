172c6bfbf3833de67ba0582fa1767711928b4336
Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
2014-09-15 17:24:38 +0200

    Add AddressType

---
+++ src/JLM/ContactBundle/Form/Type/AddressType.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+ * This file is part of the  package.
+ *
+ * (c) Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace JLM\ContactBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+/**
+ * @author Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
+ */
+class AddressType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('street',null,array('label'=>'Adresse','attr'=>array('placeholder'=>'Adresse')))
+            ->add('city','jlm_contact_city_select' ,array('label'=>'Ville','attr'=>array('placeholder'=>'Ville'))); 
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'address';
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+	public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+    	$resolver
+    		->setDefaults(array(
+            	'data_class' => 'JLM\ContactBundle\Entity\Address',
+        ));
+    }
+}
