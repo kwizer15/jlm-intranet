fd34dd930d5ccd9101909c40dc2bff29a92573a3
Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
2014-09-29 11:25:53 +0200

    Types for Person and Company

---
+++ src/JLM/ContactBundle/Form/Type/CompanyType.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+ * This file is part of the JLMContactBundle package.
+ *
+ * (c) Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace JLM\ContactBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+/**
+ * @author Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
+ */
+class CompanyType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name',null,array('label'=>'Prénom','required'=>false))
+            ->add('address','jlm_contact_address',array('label'=>'Adresse','required'=>false))
+            ->add('phone',null,array('label'=>'Téléphone','required'=>false,'attr'=>array('class'=>'input-medium')))
+            ->add('fax',null,array('label'=>'Fax','required'=>false,'attr'=>array('class'=>'input-medium')))
+            ->add('email','email',array('label'=>'Adresse e-mail','required'=>false,'attr'=>array('class'=>'input-xlarge')))
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'jlm_contact_company';
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver
+        ->setDefaults(array(
+            'data_class' => 'JLM\ContactBundle\Entity\Company',
+        ));
+    }
+}
