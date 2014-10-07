9b3523aa819272d10b9d451008115284b5a9ca0a
Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
2014-10-07 16:41:17 +0200

    Generate InstallationBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -31,6 +31,7 @@ class AppKernel extends Kernel
             new JLM\FeeBundle\JLMFeeBundle(),
             new JLM\ContactBundle\JLMContactBundle(),
             new JLM\CondominiumBundle\JLMCondominiumBundle(),
+            new JLM\InstallationBundle\JLMInstallationBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/JLM/InstallationBundle/JLMInstallationBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace JLM\InstallationBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class JLMInstallationBundle extends Bundle
+{
+}
