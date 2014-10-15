a121731f31aba49aef570c97b72de0c8a133774f
Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
2014-10-15 13:58:03 +0200

    Add comments to JLMContactBundle

--- src/JLM/ContactBundle/JLMContactBundle.php
+++ src/JLM/ContactBundle/JLMContactBundle.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the JLMContactBundle package.
+ *
+ * (c) Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace JLM\ContactBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
@@ -7,7 +16,9 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 use JLM\ContactBundle\DependencyInjection\Compiler\FormPass;
 
-
+/**
+ * @author Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
+ */
 class JLMContactBundle extends Bundle
 {
     /**
