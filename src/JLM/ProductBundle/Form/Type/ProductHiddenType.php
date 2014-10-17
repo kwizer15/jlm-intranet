5146c363c829de7b2b7d7b3e821b8ce9ad3ad730
Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
2014-10-17 17:12:15 +0200

    Product forms into ProductBundle

---
+++ src/JLM/ProductBundle/Form/Type/ProductHiddenType.php
@@ -0,0 +1,73 @@
+<?php
+
+/*
+ * This file is part of the JLMContactBundle package.
+ *
+ * (c) Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace JLM\ProductBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use JLM\ProductBundle\Form\DataTransformer\ProductToIntTransformer;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+/**
+ * @author Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
+ */
+class ProductHiddenType extends AbstractType
+{
+	/**
+	 * @var ObjectManager
+	 */
+	private $om;
+	
+	/**
+	 * @param ObjectManager $om
+	 */
+	public function __construct(ObjectManager $om)
+	{
+		$this->om = $om;
+	}
+	
+	/**
+	 * {@inheritdoc}
+	 */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+    	$transformer = new ProductToIntTransformer($this->om);
+    	$builder->addModelTransformer($transformer);
+    	
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getParent()
+    {
+    	return 'hidden';
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'jlm_product_product_hidden';
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+	public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'invalid_message' => 'The selected product does not exist',
+        ));
+    }
+}
---
+++ src/JLM/ProductBundle/Form/Type/ProductType.php
@@ -0,0 +1,62 @@
+<?php
+
+/*
+ * This file is part of the JLMContactBundle package.
+ *
+ * (c) Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace JLM\ProductBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+/**
+ * @author Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
+ */
+class ProductType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+       		->add('reference',null,array('label'=>'Référence','attr'=>array('class'=>'input-small')))
+//          ->add('barcode',null,array('label'=>'Code barre','required'=>false,'attr'=>array('class'=>'input-xlarge')))
+            ->add('category',null,array('label'=>'Famille de produit'))
+            ->add('designation',null,array('label'=>'Designation','attr'=>array('class'=>'input-xxlarge')))
+            ->add('description',null,array('label'=>'Description longue','required'=>false,'attr'=>array('class'=>'input-xxlarge')))
+            ->add('supplier',null,array('label'=>'Fournisseur')) // Typeahead
+            ->add('unity',null,array('label'=>'Unité','attr'=>array('class'=>'input-small')))
+			->add('purchase','money',array('label'=>'Prix d\'achat HT','grouping'=>true,'attr'=>array('class'=>'input-small')))
+            ->add('discountSupplier','percent',array('type'=>'integer','label'=>'Remise fournisseur','attr'=>array('class'=>'input-mini')))
+            ->add('expenseRatio','percent',array('type'=>'integer','label'=>'Frais','attr'=>array('class'=>'input-mini')))
+            ->add('shipping','money',array('label'=>'Port','grouping'=>true,'attr'=>array('class'=>'input-mini')))
+            ->add('unitPrice','money',array('label'=>'PVHT','grouping'=>true,'attr'=>array('class'=>'input-mini')))
+            
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'jlm_product_product';
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver
+        ->setDefaults(array(
+            'data_class' => 'JLM\ProductBundle\Entity\Product',
+        ));
+    }
+}
