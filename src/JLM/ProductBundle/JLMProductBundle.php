0609c33b0d57ae577bb5c81ff3c39b73bb207958
Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
2014-10-17 17:16:54 +0200

    Form view producttype

--- src/JLM/ProductBundle/JLMProductBundle.php
+++ src/JLM/ProductBundle/JLMProductBundle.php
@@ -3,7 +3,16 @@
 namespace JLM\ProductBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use JLM\ProductBundle\DependencyInjection\Compiler\FormPass;
 
 class JLMProductBundle extends Bundle
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+        $container->addCompilerPass(new FormPass());
+    }
 }
