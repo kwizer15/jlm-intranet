b143a0d1d7ab8f47e2d0fa1cb6a35437f9d51e9e
Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
2014-11-04 13:48:56 +0100

    Moving ContractController + new router

---
+++ src/JLM/ContractBundle/Controller/ContractController.php
@@ -0,0 +1,239 @@
+<?php
+
+/*
+ * This file is part of the JLMContractBundle package.
+ *
+ * (c) Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace JLM\ContractBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use JMS\SecurityExtraBundle\Annotation\Secure;
+use JLM\ContractBundle\Entity\Contract;
+use JLM\ModelBundle\Entity\Door;
+use JLM\FeeBundle\Entity\Fee;
+use JLM\ModelBundle\Form\Type\ContractType;
+use JLM\ModelBundle\Form\Type\ContractStopType;
+
+/**
+ * @author Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
+ */
+class ContractController extends Controller
+{
+    /**
+     * Lists all Contract entities.
+     *
+     * @Template()
+     * @Secure(roles="ROLE_USER")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $em->getRepository('JLMContractBundle:Contract')->findAll();
+
+        return array('entities' => $entities);
+    }
+
+    /**
+     * Finds and displays a Contract entity.
+     *
+     * @Template()
+     * @Secure(roles="ROLE_USER")
+     */
+    public function showAction(Contract $entity)
+    {
+        return array(
+            'entity'      => $entity,
+        );
+    }
+    
+    /**
+     * Displays a form to create a new Contract entity.
+     *
+     * @Template()
+     * @Secure(roles="ROLE_USER")
+     */
+    public function newAction(Door $door)
+    {
+        $entity = new Contract();
+        if (!empty($door))
+        {
+        	$entity->setDoor($door);
+        	$entity->setTrustee($door->getSite()->getTrustee());
+        }
+  
+        $entity->setBegin(new \DateTime);
+        $form   = $this->createNewForm($entity);
+
+        return array(
+            'entity' => $entity,
+            'form'   => $form->createView()
+        );
+    }
+
+    /**
+     * Creates a new Contract entity.
+     *
+     * @Template("JLMModelBundle:Contract:new.html.twig")
+     * @Secure(roles="ROLE_USER")
+     */
+    public function createAction(Request $request)
+    {
+        $entity  = new Contract();
+        $form    = $this->createNewForm($entity);
+        $form->handleRequest($request);
+
+        if ($form->isValid())
+        {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            
+            // Temporaire : se fera jour par jour
+            // ***********************************
+            if ($entity->getInProgress())
+            {
+            	$fee = new Fee();
+            	$fee->addContract($entity);
+            	$fee->setTrustee($entity->getTrustee());
+            	$fee->setAddress($entity->getDoor()->getSite()->getAddress()->toString());
+            	$fee->setPrelabel($entity->getDoor()->getSite()->getBillingPrelabel());
+            	$fee->setVat($entity->getDoor()->getSite()->getVat());
+            	$em->persist($fee);
+            }
+            //***************************************
+            
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('door_show', array('id' => $entity->getDoor()->getId())));
+            
+        }
+
+        return array(
+            'entity' => $entity,
+            'form'   => $form->createView()
+        );
+    }
+
+    /**
+     * Stop a contract
+     * 
+     * @Template("JLMModelBundle:Contract:stop.html.twig")
+     * @Secure(roles="ROLE_USER")
+     */
+    public function stopupdateAction(Contract $entity)
+    {
+    	$editForm   = $this->createStopForm($entity);
+        $request = $this->getRequest();
+        $editForm->handleRequest($request);
+
+        if ($editForm->isValid())
+        {
+        	$em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('door_show', array('id' => $entity->getDoor()->getId())));
+        }
+
+        return array(
+            'entity'      => $entity,
+            'form'   => $editForm->createView(),
+        );
+    }
+    
+    /**
+     * Stop a contract
+     *
+     * @Template()
+     * @Secure(roles="ROLE_USER")
+     */
+    public function stopAction(Contract $entity)
+    {
+    	$form = $this->createStopForm($entity);
+    	
+    	return array(
+    			'entity'      => $entity,
+    			'form'   => $form->createView(),
+    	);
+    }
+    
+    /**
+     * Displays a form to edit an existing Contract entity.
+     *
+     * @Template()
+     * @Secure(roles="ROLE_USER")
+     */
+    public function editAction(Contract $entity)
+    {
+        $editForm    = $this->createEditForm($entity);
+        
+        return array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+        );
+    }
+
+    /**
+     * Edits an existing Contract entity.
+     *
+     * @Template("JLMModelBundle:Contract:edit.old.html.twig")
+     * @Secure(roles="ROLE_USER")
+     */
+    public function updateAction(Request $request, Contract $entity)
+    {
+        $editForm = $this->createEditForm($entity);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isValid())
+        {
+        	$em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('door_show', array('id' => $entity->getDoor()->getId())));
+        }
+
+        return array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+        );
+    }
+    
+    /**
+     * Create an edit form
+     * @param Contract $entity
+     * @return \Symfony\Component\Form\Form
+     */
+    private function createEditForm(Contract $entity)
+    {
+        return $this->get('form.factory')->createNamed('contractEdit'.$entity->getId(),new ContractType(), $entity);
+    }
+    
+    /**
+     * Create a stop form
+     * @param Contract $entity
+     * @return \Symfony\Component\Form\Form
+     */
+    private function createStopForm(Contract $entity)
+    {
+        return $this->get('form.factory')->createNamed('contractStop'.$entity->getId(),new ContractStopType(), $entity);
+    }
+    
+    /**
+     * Create a new form
+     * @param Contract $entity
+     * @return \Symfony\Component\Form\Form
+     */
+    private function createNewForm(Contract $entity)
+    {
+        return $this->createForm(new ContractType(), $entity);
+    }
+    
+}
