b7b7da180bcde68914566aab8e2c496ffc206943
Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
2014-11-14 16:45:16 +0100

    Comment correction on AskBundle ConfigurationTest

--- src/JLM/AskBundle/Tests/DependencyInjection/ConfigurationTest.php
+++ src/JLM/AskBundle/Tests/DependencyInjection/ConfigurationTest.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * This file is part of the JLMContactBindle package.
+ * This file is part of the JLMContactBundle package.
  *
  * (c) Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
  *
