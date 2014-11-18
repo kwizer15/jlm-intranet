c3efb251c3d842d23d3845ca9e1da9d9eaf085e7
Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
2014-11-18 09:43:30 +0100

    Moving form attrs into view (PersonType)

--- src/JLM/ContactBundle/Form/Type/PersonType.php
+++ src/JLM/ContactBundle/Form/Type/PersonType.php
@@ -26,15 +26,15 @@ class PersonType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-        	->add('title','choice',array('label'=>'Titre','choices'=>array('M.'=>'M.','Mme'=>'Mme','Mlle'=>'Mlle'),'attr'=>array('class'=>'input-small')))
+        	->add('title','choice',array('label'=>'Titre', 'choices'=>array('M.'=>'M.','Mme'=>'Mme','Mlle'=>'Mlle')))
         	->add('lastName',null,array('label'=>'Nom'))
-            ->add('firstName',null,array('label'=>'Prénom','required'=>false))
-            ->add('role',null,array('label'=>'Rôle','required'=>true))
-            ->add('address','jlm_contact_address',array('label'=>'Adresse','required'=>false))
-            ->add('fixedPhone',null,array('label'=>'Téléphone fixe','required'=>false,'attr'=>array('class'=>'input-medium')))
-            ->add('mobilePhone',null,array('label'=>'Téléphone mobile','required'=>false,'attr'=>array('class'=>'input-medium')))
-            ->add('fax',null,array('label'=>'Fax','required'=>false,'attr'=>array('class'=>'input-medium')))
-            ->add('email','email',array('label'=>'Adresse e-mail','required'=>false,'attr'=>array('class'=>'input-xlarge')))
+            ->add('firstName',null,array('label'=>'Prénom', 'required'=>false))
+            ->add('role',null,array('label'=>'Rôle', 'required'=>true))
+            ->add('address','jlm_contact_address',array('label'=>'Adresse', 'required'=>false))
+            ->add('fixedPhone',null,array('label'=>'Téléphone fixe', 'required'=>false))
+            ->add('mobilePhone',null,array('label'=>'Téléphone mobile', 'required'=>false))
+            ->add('fax',null,array('label'=>'Fax', 'required'=>false))
+            ->add('email','email',array('label'=>'Adresse e-mail', 'required'=>false))
         ;
     }
 
