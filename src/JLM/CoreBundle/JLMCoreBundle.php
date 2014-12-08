f3a11ac4cfa2ed25e54834f26f73573a1da99041
Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
2014-12-08 14:47:28 +0100

    Moving base layouts

--- src/JLM/CoreBundle/JLMCoreBundle.php
+++ src/JLM/CoreBundle/JLMCoreBundle.php
@@ -3,7 +3,17 @@
 namespace JLM\CoreBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use JLM\CoreBundle\DependencyInjection\Compiler\FormPass;
 
 class JLMCoreBundle extends Bundle
 {
+	/**
+	 * {@inheritdoc}
+	 */
+	public function build(ContainerBuilder $container)
+	{
+		parent::build($container);
+		$container->addCompilerPass(new FormPass());
+	}
 }
