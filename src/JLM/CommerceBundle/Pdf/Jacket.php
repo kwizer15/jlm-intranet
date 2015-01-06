2e4400ae60115136015ce41a2ef676ff51a25fda
Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
2015-01-06 11:58:22 +0100

    Correction impression telephones sur jaquette devis

--- src/JLM/CommerceBundle/Pdf/Jacket.php
+++ src/JLM/CommerceBundle/Pdf/Jacket.php
@@ -39,10 +39,10 @@ class Jacket extends FPDFext
 		if ($trustee)
 		{
 			$pdf->setFont('Arial','',14);
-			if ($trustee->getPhone())
-				$pdf->cell(0,7,'Tèl : '.$trustee->getPhone(),0,1);
-			if ($trustee->getFax())
-				$pdf->cell(0,7,'Fax : '.$trustee->getFax(),0,1);
+			foreach ($trustee->getPhones() as $phone)
+			{
+				$pdf->cell(0,7,$phone->getLabel().' : '.$phone->getNumber(),0,1);
+			}
 			$pdf->ln(5);
 		}
 		$pdf->setFont('Arial','BI',11);
@@ -53,12 +53,10 @@ class Jacket extends FPDFext
 		if ($contact)
 		{
 			$pdf->setFont('Arial','I',11);
-			if ($contact->getMobilePhone())
-				$pdf->cell(0,5,'Tél port. : '.$contact->getMobilePhone(),0,1);
-			if ($contact->getFixedPhone())
-				$pdf->cell(0,5,'Tél fixe : '.$contact->getFixedPhone(),0,1);
-			if ($contact->getFax())
-				$pdf->cell(0,5,'Fax : '.$contact->getFax(),0,1);
+			foreach ($contact->getPhones() as $phone)
+			{
+				$pdf->cell(0,7,$phone->getLabel().' : '.$phone->getNumber(),0,1);
+			}
 			if ($contact->getEmail())
 				$pdf->cell(0,5,'Email : '.$contact->getEmail(),0,1);
 		}
