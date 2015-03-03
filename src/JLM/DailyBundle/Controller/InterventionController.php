7b404972ca0acdc2d3e6caddb93de108fbe4e0a5
Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
2015-03-03 10:46:30 +0100

    Changement charste pour export excel

--- src/JLM/DailyBundle/Controller/InterventionController.php
+++ src/JLM/DailyBundle/Controller/InterventionController.php
@@ -505,7 +505,7 @@ class InterventionController extends Controller
 		}
 		krsort($shifts);
 		$response = new Response();
-		$response->headers->set('Content-Type', 'text/csv');
+		$response->headers->set('Content-Type', 'text/csv; charset=iso-8859-1');
 		$response->headers->set('Content-Disposition', 'inline; filename='.$door->getId().'.csv');
 		$response->setContent($this->render('JLMDailyBundle:Intervention:door.csv.twig',array(
 						'entity' => $door,
