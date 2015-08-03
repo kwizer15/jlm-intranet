2dc5cfc6e42568e6bbf55f498b20ce5f2dd628e6
Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
2015-08-03 12:24:59 +0200

    Correction bug

--- src/JLM/ModelBundle/Entity/Site.php
+++ src/JLM/ModelBundle/Entity/Site.php
@@ -24,6 +24,7 @@ use JLM\CommerceBundle\Model\BillInterface;
 use JLM\CommerceBundle\Model\VATInterface;
 use JLM\CommerceBundle\Model\BusinessInterface;
 use JLM\AskBundle\Model\SubjectInterface;
+use JLM\ContactBundle\Entity\Company;
 
 /**
  * @author Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
