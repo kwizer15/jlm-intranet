6bd54579e0cebafc343cbb60cc908931b4a4115d
Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
2015-12-01 14:11:37 +0100

    Sécurisation du e-carnet

--- src/JLM/FrontBundle/Controller/BusinessController.php
+++ src/JLM/FrontBundle/Controller/BusinessController.php
@@ -13,6 +13,9 @@ namespace JLM\FrontBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Doctrine\ORM\NoResultException;
 
 /**
  * @author Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
@@ -28,15 +31,39 @@ class BusinessController extends Controller
     {
     	//return $this->render('JLMFrontBundle:Business:list.html.twig', array());
     	$request = $this->get('request');
+
+    	$service = (Kernel::MINOR_VERSION > 3) ? 'security.token_storage' : 'security.context';
+    	$securityTokenStorage = $this->container->get($service);
+    	
+    	if (false === $securityTokenStorage->isGranted('ROLE_MANAGER'))
+    	{
+    		throw new AccessDeniedException();
+    	}
     	
     	$om = $this->get('doctrine')->getManager();
-		$manager = $om->getRepository('JLMModelBundle:Trustee')->find(111);
-		
-		$repo = $om->getRepository('JLMModelBundle:Site');
-		$sites = $repo->getByManager($manager);
+    	$repoManager = $om->getRepository('JLMModelBundle:Trustee');
+    	
+    	try {
+			$manager = $repoManager->getByUser($securityTokenStorage->getToken()->getUser());
+    	} catch (NoResultException $e) {
+    		if (false === $securityTokenStorage->isGranted('ROLE_ADMIN'))
+    		{
+    			throw new AccessDeniedException('Pas de contact lié à ce compte');
+    		}
+    		$manager = $repoManager->find(111); // Pour tests
+    	}
+		$repoSite = $om->getRepository('JLMModelBundle:Site');
+		$sites = $repoSite->getByManager($manager);
 		$activeBusinessId = sizeof($sites) ? $request->get('business', reset($sites)->getId()) : null;
+		
 		try {
-			$activeBusiness = $repo->find($activeBusinessId);
+			$activeBusiness = $repoSite->find($activeBusinessId);
+			if (!$activeBusiness->hasContractWith($manager))
+			{
+				$activeBusinessId = sizeof($sites) ? reset($sites)->getId() : null;
+				$activeBusiness = $repoSite->find($activeBusinessId);
+			}
+			// Vérifier si l'affaire fait partie du syndic
 		} catch (\Doctrine\ORM\NoResultException $e) {
 			throw $this->createNotFoundException('Cette affaire n\'existe pas');
 		}
