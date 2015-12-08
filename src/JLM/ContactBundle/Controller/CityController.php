a768181da12ee442a6249fbbf9c898372d3fe619
Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
2015-12-08 11:12:06 +0100

    Unsecure city controller (json requests)

--- src/JLM/ContactBundle/Controller/CityController.php
+++ src/JLM/ContactBundle/Controller/CityController.php
@@ -25,7 +25,6 @@ class CityController extends ContainerAware
 	public function searchAction()
 	{
 		$manager = $this->container->get('jlm_contact.city_manager');
-		$manager->secure('ROLE_OFFICE');
 		$request = $manager->getRequest();
 		$term = $request->get('q');
 		$page_limit = $request->get('page_limit');;
@@ -40,7 +39,6 @@ class CityController extends ContainerAware
 	public function jsonAction()
 	{
 		$manager = $this->container->get('jlm_contact.city_manager');
-		$manager->secure('ROLE_OFFICE');
 		$id = $manager->getRequest()->get('id');
 		$city = $manager->getRepository()->getByIdToArray($id);
 		
