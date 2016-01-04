46abf9ae3481f6d897e62efdcb927b3a274d1ddd
Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
2016-01-04 16:11:36 +0100

    Disparition de la modale après envoi du mail

--- src/JLM/FrontBundle/JLMFrontBundle.php
+++ src/JLM/FrontBundle/JLMFrontBundle.php
@@ -3,7 +3,17 @@
 namespace JLM\FrontBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use JLM\FrontBundle\DependencyInjection\Compiler\FormPass;
 
 class JLMFrontBundle extends Bundle
 {
+	/**
+	 * {@inheritdoc}
+	 */
+	public function build(ContainerBuilder $container)
+	{
+		parent::build($container);
+		$container->addCompilerPass(new FormPass());
+	}
 }
